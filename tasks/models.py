e6c98a2f3abae1947655ca6ee918790639409048
cr <chen107101@gmail.com>
2021-11-15 01:00:20 +0800

    new tables migrated

--- tasks/models.py
+++ tasks/models.py
@@ -5,14 +5,14 @@ from django.utils import timezone
 # Create your models here.
 
 class Status(models.TextChoices):
-    UNSTARTED = 'u', 'Not started'
-    ONGOING = 'o', 'ongoing'
-    FINISHED = 'f', 'Finished'
+    UNSTARTED = 'u','Not started'
+    ONGOING = 'o','ongoing'
+    FINISHED = 'f','Finished'
 
 
 class Task(models.Model):
-    name = models.CharField(verbose_name='Task Name', max_length=50, unique=True)
-    status = models.CharField(verbose_name='Status Name', max_length=1, choices=Status.choices)
+    name = models.CharField(verbose_name = 'Task Name',max_length = 50,unique = True)
+    status = models.CharField(verbose_name = 'Status Name',max_length = 1,choices = Status.choices)
 
     class Meta:
         verbose_name = 'Our Task'
@@ -23,12 +23,35 @@ class Task(models.Model):
 
 
 class Course(models.Model):
-    name = models.CharField(verbose_name='Course Name', max_length=80)
-    # teacher_id = models.ForeignKey(on_delete=models.SET_NULL, blank=False, null=True, verbose_name='Teacher ID',
-    # db_index=True)
-    weekday = models.IntegerField(verbose_name='Weekday')
-    start_time = models.TimeField(verbose_name='Start Time', auto_now=True)
-    finish_time = models.TimeField(verbose_name='Finish Time', auto_now=True)
+    cid = models.BigAutoField(primary_key = True)
+    tid = models.ForeignKey('Teacher',on_delete = models.CASCADE)
+    name = models.CharField(max_length = 50)
+    weekday = models.IntegerField()
+    start_time = models.TimeField()
+    finish_time = models.TimeField()
 
-    def __str__(self):
-        return self.name
+
+class Student(models.Model):
+    sid = models.CharField(max_length = 50,primary_key = True)
+    stu_name = models.CharField(max_length = 50)
+    img_path = models.CharField(max_length = 50,unique = True)
+
+
+class Teacher(models.Model):
+    tid = models.BigAutoField(primary_key = True)
+    name = models.CharField(max_length = 50)
+    account = models.CharField(max_length = 50,unique = True)
+    pwd = models.CharField(max_length = 50)
+
+
+class SignIn(models.Model):
+    sid = models.ForeignKey('Student',on_delete = models.CASCADE)
+    cid = models.ForeignKey('Course',on_delete = models.CASCADE)
+    time = models.DateTimeField(auto_now = True)
+
+
+class TakeClass(models.Model):
+    sid = models.ForeignKey('Student',on_delete = models.CASCADE)
+    cid = models.ForeignKey('Course',on_delete = models.CASCADE)
+    class Meta:
+        unique_together=(("sid","cid"),)
