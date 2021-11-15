cb8d9d6d4a66cd6ea4e8b3a2076f3647fd9c9875
cr <chen107101@gmail.com>
2021-11-15 10:25:35 +0800

    finish sign-in record page

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,7 +8,7 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('api/',views.api,name='api'),
+    re_path('api/*',views.api,name='api'),
     path('sign_in_records/', views.sign_in_records, name='sign_in_records'),
     path('retroactive_records/', views.retroactive_records, name='retroactive_records'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .models import Task ,SignIn ,Course ,Student ,Teacher ,TakeClass
+from .models import Task, SignIn, Course, Student, Teacher, TakeClass
 import json
+import datetime
+
 
 # Create your views here.
 def dashboard(request):
     user_count = User.objects.count()
     task_count = Task.objects.count()
 
-    context = {'user_count': user_count ,'task_count': task_count}
-    return render(request ,'tasks/dashboard.html' ,context)
+    context = {'user_count': user_count, 'task_count': task_count}
+    return render(request, 'tasks/dashboard.html', context)
 
 
 def sign_in_records(request):
@@ -19,46 +21,46 @@ def sign_in_records(request):
     student = Student.objects.all()
     take_class = TakeClass.objects.all()
 
-    context = {'all_course': course ,
-               'all_sign_in': sign_in ,
-               'all_students': student ,
+    context = {'all_course': course,
+               'all_sign_in': sign_in,
+               'all_students': student,
                'all_take_class': take_class}
-    return render(request ,'tasks/sign_in.html' ,context)
+    return render(request, 'tasks/sign_in.html', context)
 
 
 def retroactive_records(request):
-    return render(request ,'tasks/retroactive.html')
+    return render(request, 'tasks/retroactive.html')
 
 
 def api(request):
     cid = request.GET.get('cid')
-
-    course = Course.objects.all().filter(cid=cid)[0]
-    take_class = TakeClass.objects.all().filter(cid=cid)
+    course = Course.objects.all().filter(cid = cid)[0]
+    take_class = TakeClass.objects.all().filter(cid = cid)
+    total_student = len(take_class)
 
     start_time = course.start_time
     finish_time = course.finish_time
 
-    date = request.GET.get('date')
-    print(date)
-    # start=date+start_time
-    # end=date+finish_time
-    # print(start)
-    # print(end)
-    # sign_in = SignIn.objects.all().filter(cid=cid,time__gte=start,time__lte=end)
-    # print(sign_in)
+    date = datetime.datetime.strptime(request.GET.get('date'), '%Y-%m-%d')
+    start = datetime.datetime(date.year, date.month, date.day,
+                              start_time.hour,
+                              start_time.minute,
+                              start_time.second)
+    end = datetime.datetime(date.year, date.month, date.day,
+                            finish_time.hour,
+                            finish_time.minute,
+                            finish_time.second)
+    sign_in = SignIn.objects.all().filter(cid = cid, time__range = [date, start])
 
-    total_student=len(take_class)
-    # for row in sign_in:
-    #     print(row.time)
-    #     if row.time<finish_time and row.time>finish_time:
-    #         print(row)
+    signed = []
+    for row in sign_in:
+        signed.append(row.sid)
+    sign_in_num = len(set(signed))
 
-    sign_in_num=1
     statistics = {
-        'total_student':total_student,
-        'sign_in_num':sign_in_num,
-        'not_sign_in_num':total_student-sign_in_num,
-        'percentage':round(sign_in_num/total_student*100)
+        'total_student': total_student,
+        'sign_in_num': sign_in_num,
+        'not_sign_in_num': total_student - sign_in_num,
+        'percentage': round(sign_in_num / total_student * 100)
     }
     return HttpResponse(json.dumps(statistics))
